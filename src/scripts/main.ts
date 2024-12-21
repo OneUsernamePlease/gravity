3aedc07bb4efae74c47c47187b8faa30e90485e4
Jo <j.gugerbauer@gmail.com>
2024-12-21 13:07:51 +0100

    Add some more basic functionality

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,14 +1,34 @@
 document.addEventListener("DOMContentLoaded", initialize);
+let offscreenCanvas: OffscreenCanvas; //use this in a worker thread to render or draw on, then transfer content to the visible html-canvas
+let visibleCanvas: HTMLCanvasElement;
+let offscreenCanvasCtx: OffscreenCanvasRenderingContext2D;
+let visibleCanvasCtx: CanvasRenderingContext2D
+let statusBar: HTMLElement;
 function initialize() {
     registerEvents();
+    initCanvas();
 
+    statusBar = document.getElementById("statusText")!;
     document.removeEventListener("DOMContentLoaded", initialize);
 }
 function registerEvents() {
     document.getElementById("canvasBtn1")?.addEventListener("click", genericTest)
 }
 function genericTest() {
-    alert("this is a test")
+    setStatusMessage("drawing something");
+
+    offscreenCanvasCtx.fillStyle = 'blue';
+    offscreenCanvasCtx.fillRect(0, 0, 25, 25);
+    offscreenCanvasCtx.fillStyle = 'green';
+    offscreenCanvasCtx.fillRect(10, 10, 10, 10);
+    visibleCanvasCtx.drawImage(offscreenCanvas, 0, 0);
 }
-
-
+function initCanvas() {
+    visibleCanvas = (document.getElementById("theCanvas")) as HTMLCanvasElement;
+    visibleCanvasCtx = visibleCanvas.getContext("2d")!;
+    offscreenCanvas = new OffscreenCanvas(visibleCanvas.clientWidth, visibleCanvas.clientHeight);
+    offscreenCanvasCtx = offscreenCanvas.getContext("2d")!;
+}
+function setStatusMessage(newMessage: string) {
+    statusBar.innerHTML = newMessage;
+}
--- src/scripts/out/main.js
+++ src/scripts/out/main.js
@@ -1,7 +1,14 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", initialize);
+let offscreenCanvas; //use this in a worker thread to render or draw on, then transfer content to the visible html-canvas
+let visibleCanvas;
+let offscreenCanvasCtx;
+let visibleCanvasCtx;
+let statusBar;
 function initialize() {
     registerEvents();
+    initCanvas();
+    statusBar = document.getElementById("statusText");
     document.removeEventListener("DOMContentLoaded", initialize);
 }
 function registerEvents() {
@@ -9,5 +16,19 @@ function registerEvents() {
     (_a = document.getElementById("canvasBtn1")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", genericTest);
 }
 function genericTest() {
-    alert("this is a test");
+    setStatusMessage("drawing something");
+    offscreenCanvasCtx.fillStyle = 'blue';
+    offscreenCanvasCtx.fillRect(0, 0, 25, 25);
+    offscreenCanvasCtx.fillStyle = 'green';
+    offscreenCanvasCtx.fillRect(10, 10, 10, 10);
+    visibleCanvasCtx.drawImage(offscreenCanvas, 0, 0);
+}
+function initCanvas() {
+    visibleCanvas = (document.getElementById("theCanvas"));
+    visibleCanvasCtx = visibleCanvas.getContext("2d");
+    offscreenCanvas = new OffscreenCanvas(visibleCanvas.clientWidth, visibleCanvas.clientHeight);
+    offscreenCanvasCtx = offscreenCanvas.getContext("2d");
+}
+function setStatusMessage(newMessage) {
+    statusBar.innerHTML = newMessage;
 }
