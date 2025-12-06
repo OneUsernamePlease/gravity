c3e3cff98a0776580a977f4b3a9d19f59deece2b
Jo <j.gugerbauer@gmail.com>
2025-12-06 12:34:44 +0100

    removed most control-methods, UI does not need those any more

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,7 +1,5 @@
-import { Vector2D } from "./vector2d";
 import { UI } from "./ui";
 import { GravityAnimationController } from "./gravity-animation-controller";
-import * as c from "../const";
 
 export class App {
     private _ui: UI;
@@ -16,21 +14,12 @@ export class App {
     get ui() {
         return this._ui;
     }
-    get running() {
-        return this.gravityAnimationController.running;
-    }
-    // additional getters
-    get selectedClickAction() {
-        return this.ui.selectedClickAction;
-    }
     //#endregion
     constructor() {
         this._gravityAnimationController = new GravityAnimationController(this);        
-        this._ui = new UI(this);
-
-        this.initialize();
+        this._ui = new UI(this.gravityAnimationController);
     }
-    private initialize() {
+    public initialize() {
         const width = window.innerWidth;
         const height = window.innerHeight;
         this.gravityAnimationController.initialize(width, height);
@@ -44,13 +33,6 @@ export class App {
     public stop() {
         this.gravityAnimationController.stop();
         this.ui.simulationStopped();
-    }
-    public toggleSimulation() {
-        if (this.running) {
-            this.stop();
-        } else {
-            this.run();
-        }
     }
     public advanceOneTick() {
         this.gravityAnimationController.advanceOneTick();
@@ -71,44 +53,9 @@ export class App {
         this.ui.updateStatusBarZoom(this.gravityAnimationController.currentZoom);
     }
     //#endregion
-    
-    //#region interaction
-    public scrollViewRight(distance?: number) {
-        this.gravityAnimationController.scrollRight(distance);
-    }
-    public scrollViewLeft(distance?: number) {
-        this.gravityAnimationController.scrollLeft(distance);
-    }
-    public scrollViewUp(distance?: number) {
-        this.gravityAnimationController.scrollUp(distance);
-    }
-    public scrollViewDown(distance?: number) {
-        this.gravityAnimationController.scrollDown(distance);
-    }
-    public setG(g: number) {
-        this.gravityAnimationController.setG(g);
-    }
-    public zoomOut(zoomCenter?: Vector2D, zoomFactor: number = c.DEFAULT_ZOOM_FACTOR): number {
-        const newZoom = this.gravityAnimationController.zoomOutByFactor(zoomCenter, zoomFactor);
-        return newZoom;
-    }
-    public zoomIn(zoomCenter?: Vector2D, zoomFactor: number = c.DEFAULT_ZOOM_FACTOR) {
-        const newZoom = this.gravityAnimationController.zoomInByFactor(zoomCenter, zoomFactor);
-        return newZoom;
-    }
-    public resizeCanvas(width: number, height: number) {
-        this.gravityAnimationController.resizeCanvas(width, height);
-        this.ui.updateStatusBarCanvasDimensions(width, height);
-    }
-    public setCollisionDetection(collisionDetection: boolean, elasticCollisions = false) {
-        this.gravityAnimationController.simulation.collisionDetection = collisionDetection;
-        this.gravityAnimationController.simulation.elasticCollisions = elasticCollisions;
-    }
-    public setElasticCollisions(elasticCollisions: boolean) {
-        this.gravityAnimationController.simulation.elasticCollisions = elasticCollisions;
-    }
-    public setDisplayVectors(displayVectors: boolean) {
-        this.gravityAnimationController.setDisplayVectors(displayVectors)
+    public resizeCanvas(windowWidth: number, windowHeight: number) {
+        this.gravityAnimationController.resizeCanvas(windowWidth, windowHeight);
+        this.ui.updateStatusBarCanvasDimensions(windowWidth, windowHeight);
     }
 }
-    //#endregion
+//#endregion
