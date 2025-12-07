4d1ff4bd23207578df47d32bd89f7b885688eadc
Jo <j.gugerbauer@gmail.com>
2025-12-07 09:47:52 +0100

    add function massDependentColor()

--- src/scripts/animation/animation-utils.ts
+++ src/scripts/animation/animation-utils.ts
@@ -1,21 +1,21 @@
-import { rng } from "../util/util";
+import { rng, twoDigitHex } from "../util/util";
 /**
  * 
  * @param alpha value between 0 and 1. Defaults to 1 (opaque).
  * @returns a random hex-color-string in the format #RRGGBBAA
- */
+ */ 
 export function randomHexColor(alpha: number = 1): string {
-  alpha = Math.min(1, Math.max(0, alpha));
-  const a = Math.round(alpha * 255);
+    alpha = Math.min(1, Math.max(0, alpha));
+    const a = Math.round(alpha * 255);
 
-  const toHex = (n: number) => n.toString(16).padStart(2, "0");
+    const toHex = (n: number) => n.toString(16).padStart(2, "0");
 
-  // Random RGB values
-  const r = rng(0, 256);
-  const g = rng(0, 256);
-  const b = rng(0, 256);
 
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(a)}`;
+    const r = rng(0, 256);
+    const g = rng(0, 256);
+    const b = rng(0, 256);
+
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(a)}`;
 }
 /**
  * !!! COLORS MUST BE IN THE FORMAT #RRGGBBAA !!!
@@ -34,9 +34,6 @@ export function mixColorsWeighted(color1: string, weight1: number, color2: strin
         return [r, g, b, a];
     }
 
-    // produce 2-digit hex
-    const hex = (n: number) => n.toString(16).padStart(2, "0");
-
     const [r1, g1, b1, a1] = parseHex(color1);
     const [r2, g2, b2, a2] = parseHex(color2);
 
@@ -45,5 +42,35 @@ export function mixColorsWeighted(color1: string, weight1: number, color2: strin
     const b = Math.round((b1 * weight1 + b2 * weight2) / (weight1 + weight2));
     const a = Math.round((a1 * weight1 + a2 * weight2) / (weight1 + weight2));
 
-    return `#${hex(r)}${hex(g)}${hex(b)}${hex(a)}`;
+    return `#${twoDigitHex(r)}${twoDigitHex(g)}${twoDigitHex(b)}${twoDigitHex(a)}`;
+}
+export function massDependentColor(mass: number): string {
+    const lowestColorMass = 1000;
+    const biggestColorMass = 100000000;
+    const alpha = "ff";
+
+    if (mass <= lowestColorMass) {
+        return "#ff0000ff";
+    }
+    if (mass >= biggestColorMass) {
+        return "#0000ffff";
+    }
+    const mid = Math.sqrt(lowestColorMass * biggestColorMass)
+    let r,g,b;
+
+    if (mass < mid) {
+        // Red -> White
+        const t = (Math.log(mass) - Math.log(lowestColorMass)) / (Math.log(mid) - Math.log(lowestColorMass));
+        r = 255;
+        g = Math.round(255 * t);
+        b = Math.round(255 * t);
+    } else {
+        // White -> Blue
+        const t = (Math.log(mass) - Math.log(mid)) / (Math.log(biggestColorMass) - Math.log(mid));
+        r = Math.round(255 * (1 - t));
+        g = Math.round(255 * (1 - t));
+        b = 255;
+    }
+
+    return `#${twoDigitHex(r)}${twoDigitHex(g)}${twoDigitHex(b)}${alpha}`;
 }
