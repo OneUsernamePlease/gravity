1538f5764a173c2b88d9ba52db41c7b3ba1fd3df
Jo <j.gugerbauer@gmail.com>
2026-04-15 22:32:31 +0200

    updates and config changes

--- src/tests/gravity.spec.ts
+++ src/tests/gravity.spec.ts
@@ -1,13 +1,13 @@
-import { test } from "vitest";
-import { Body2d } from "../scripts/simulation/gravity";
-import { Gravity } from "../scripts/simulation/gravity-controller";
+import { expect, test } from "vitest";
+import { Body2d } from "../scripts/simulation/body2d";
+import { Gravity } from "../scripts/simulation/gravity";
 import { Vector2D } from "../scripts/util/vector2d";
 
-test("elastic collision, two bodies, one moving towards other center", () => {
-
+test("elastic collision, two bodies, one moving towards other center", ({skip}) => {
+    skip();
+    // using bodies requires some more config...
     const sim = new Gravity();
-    const body1 = sim.addObject(new Body2d(100), new Vector2D(0, 0), new Vector2D(0, 0)); // stationary
-    const body2 = sim.addObject(new Body2d(100), new Vector2D(0, 0), new Vector2D(0, 0)); // moving towards body1
-    
+    const body1 = sim.addObject({body: new Body2d(100), position: new Vector2D(0, 0), velocity: new Vector2D(0, 0), acceleration: new Vector2D(0, 0)}); // stationary
+    const body2 = sim.addObject({body: new Body2d(100), position: new Vector2D(0, 0), velocity: new Vector2D(0, 0), acceleration: new Vector2D(0, 0)}); // moving towards body1
 });
 
