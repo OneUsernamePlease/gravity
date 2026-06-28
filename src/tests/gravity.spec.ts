0c1bcaa58d6c1571d5a57c726cbafed9ed5d5fc2
Jo <j.gugerbauer@gmail.com>
2026-06-28 13:10:07 +0200

    mock CSS

--- src/tests/gravity.spec.ts
+++ src/tests/gravity.spec.ts
@@ -1,10 +1,21 @@
-import { expect, test } from "vitest";
+import { afterAll, beforeAll, expect, test, vi } from "vitest";
 import { Body2d } from "../scripts/simulation/body2d";
 import { Gravity } from "../scripts/simulation/gravity";
 import { Vector2D } from "../scripts/util/vector2d";
 
-test("elastic collision, two bodies, one moving towards other center", ({skip}) => {
-    skip();
+beforeAll(() => {
+    Object.defineProperty(globalThis, "CSS", {
+        value: {
+            supports: vi.fn(() => true),
+        },
+    });
+});
+
+afterAll(() => {
+    vi.restoreAllMocks();
+})
+
+test("elastic collision, two bodies, one moving towards other center", ({}) => {
     // using bodies requires some more config...
     const sim = new Gravity();
     const body1 = sim.addObject({body: new Body2d(100), position: new Vector2D(0, 0), velocity: new Vector2D(0, 0), acceleration: new Vector2D(0, 0)}); // stationary
